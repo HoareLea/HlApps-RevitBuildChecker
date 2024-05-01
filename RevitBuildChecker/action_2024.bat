a66db240f7b29015a8467475c8ebf537829d4a3c
CharlieHL <charliejones@hoarelea.com>
2024-05-01 14:12:32 +0100

    rem

--- RevitBuildChecker/action_2024.bat
+++
@@ -1,6 +0,0 @@
-copy /y "C:\Users\jonesc4\source\repos\RevitBuildChecker\RevitBuildChecker\dist\RevitBuildChecker.addin" "C:\ProgramData\Autodesk\Revit\Addins\2024\RevitBuildChecker.addin"
-copy /y "C:\Users\jonesc4\source\repos\RevitBuildChecker\RevitBuildChecker\dist\RevitBuildChecker.dll" "C:\ProgramData\Autodesk\Revit\Addins\2024\RevitBuildChecker.dll"
-pause
-start "" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Autodesk\Revit 2024\Revit 2024.lnk"
-pause
-taskkill /im revit.exe /f /t 
--- RevitBuildChecker/rem_2023.bat
+++
@@ -1,3 +0,0 @@
-del "C:\ProgramData\Autodesk\Revit\Addins\2023\CJ_test_Class_Library.dll" /f
-del "C:\ProgramData\Autodesk\Revit\Addins\2023\CJ_test_Class_library.addin" /f
-pause
