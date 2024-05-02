6b5191314cf8a9f387047ef24e2343b29e3cb986
Charlie Jones <66369495+CharlieHL@users.noreply.github.com>
2024-05-02 18:30:41 +0100

    Update ACTION_ALL.bat

--- RevitBuildChecker/dist/ACTION_ALL.bat
+++ RevitBuildChecker/dist/ACTION_ALL.bat
@@ -1,25 +1,62 @@
-cd C:\SourceFiles\dist
-copy /y "RevitVersionsInfo.json" "C:\SourceFiles\HLApps-Deployment-master\RevitVersionsInfo.json"
-copy /y "RevitBuildChecker.addin" "C:\ProgramData\Autodesk\Revit\Addins\2020\RevitBuildChecker.addin"
-copy /y "RevitBuildChecker.dll" "C:\ProgramData\Autodesk\Revit\Addins\2020\RevitBuildChecker.dll"
+@echo off
+:: Change directory to the build output folder
+cd /d "C:\Users\jonesc4\source\repos\HoareLea\HlApps-RevitBuildChecker\RevitBuildChecker\dist"
 
-copy /y "RevitVersionsInfo.json" "C:\SourceFiles\HLApps-Deployment-master\RevitVersionsInfo.json"
-copy /y "RevitBuildChecker.addin" "C:\ProgramData\Autodesk\Revit\Addins\2021\RevitBuildChecker.addin"
-copy /y "RevitBuildChecker.dll" "C:\ProgramData\Autodesk\Revit\Addins\2021\RevitBuildChecker.dll"
+:: Check if the deployment directory exists and copy JSON file if it does
+if exist "C:\SourceFiles\HLApps-Deployment-master\" (
+    copy /y "RevitVersionsInfo.json" "C:\SourceFiles\HLApps-Deployment-master\RevitVersionsInfo.json" && (
+        echo Successfully copied RevitVersionsInfo.json to deployment directory.
+    ) || (
+        echo ERROR: Failed to copy RevitVersionsInfo.json.
+    )
+) else (
+    :: If it doesn't exist, create the directory and copy necessary files
+    mkdir "C:\SourceFiles\HLApps-Deployment-master\" && (
+        echo Created directory: C:\SourceFiles\HLApps-Deployment-master\
+    )
+    xcopy /d /y *.dll "C:\SourceFiles\HLApps-Deployment-master\RevitBuildChecker\" && (
+        echo DLL files copied to RevitBuildChecker directory.
+    ) || (
+        echo ERROR: Failed to copy DLL files.
+    )
+)
 
-copy /y "RevitVersionsInfo.json" "C:\SourceFiles\HLApps-Deployment-master\RevitVersionsInfo.json"
-copy /y "RevitBuildChecker.addin" "C:\ProgramData\Autodesk\Revit\Addins\2022\RevitBuildChecker.addin"
-copy /y "RevitBuildChecker.dll" "C:\ProgramData\Autodesk\Revit\Addins\2022\RevitBuildChecker.dll"
+:: Define a base path for easier reference
+set "ADDINS_PATH=C:\ProgramData\Autodesk\Revit\Addins"
 
-copy /y "RevitVersionsInfo.json" "C:\SourceFiles\HLApps-Deployment-master\RevitVersionsInfo.json"
-copy /y "RevitBuildChecker.addin" "C:\ProgramData\Autodesk\Revit\Addins\2023\RevitBuildChecker.addin"
-copy /y "RevitBuildChecker.dll" "C:\ProgramData\Autodesk\Revit\Addins\2023\RevitBuildChecker.dll"
+:: Loop through each year from 2020 to 2025
+for %%Y in (2020 2021 2022 2023 2024 2025) do (
+    set "YEAR_PATH=%%ADDINS_PATH%%\%%Y\RevitBuildChecker"
+    
+    :: Check if the directory for the current year exists
+    if exist "%%YEAR_PATH%%" (
+        copy /y "RevitBuildChecker.addin" "%%YEAR_PATH%%\RevitBuildChecker.addin" && (
+            echo RevitBuildChecker.addin copied to %%Y.
+        ) || (
+            echo ERROR: Failed to copy RevitBuildChecker.addin to %%Y.
+        )
+        copy /y "RevitBuildChecker.dll" "%%YEAR_PATH%%\RevitBuildChecker.dll" && (
+            echo RevitBuildChecker.dll copied to %%Y.
+        ) || (
+            echo ERROR: Failed to copy RevitBuildChecker.dll to %%Y.
+        )
+    ) else (
+        :: If it doesn't exist, create the directory and copy the files
+        mkdir "%%YEAR_PATH%%" && (
+            echo Created directory: %%YEAR_PATH%%
+        )
+        copy /y "RevitBuildChecker.addin" "%%YEAR_PATH%%\RevitBuildChecker.addin" && (
+            echo RevitBuildChecker.addin copied to newly created %%Y directory.
+        ) || (
+            echo ERROR: Failed to copy RevitBuildChecker.addin to newly created %%Y directory.
+        )
+        copy /y "RevitBuildChecker.dll" "%%YEAR_PATH%%\RevitBuildChecker.dll" && (
+            echo RevitBuildChecker.dll copied to newly created %%Y directory.
+        ) || (
+            echo ERROR: Failed to copy RevitBuildChecker.dll to newly created %%Y directory.
+        )
+    )
+)
 
-copy /y "RevitVersionsInfo.json" "C:\SourceFiles\HLApps-Deployment-master\RevitVersionsInfo.json"
-copy /y "RevitBuildChecker.addin" "C:\ProgramData\Autodesk\Revit\Addins\2024\RevitBuildChecker.addin"
-copy /y "RevitBuildChecker.dll" "C:\ProgramData\Autodesk\Revit\Addins\2024\RevitBuildChecker.dll"
-
-copy /y "RevitVersionsInfo.json" "C:\SourceFiles\HLApps-Deployment-master\RevitVersionsInfo.json"
-copy /y "RevitBuildChecker.addin" "C:\ProgramData\Autodesk\Revit\Addins\2025\RevitBuildChecker.addin"
-copy /y "RevitBuildChecker.dll" "C:\ProgramData\Autodesk\Revit\Addins\2025\RevitBuildChecker.dll"
+:: Pause to view any messages or errors
 pause
