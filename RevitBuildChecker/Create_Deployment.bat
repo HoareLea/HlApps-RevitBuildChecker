21bc358a209242a7bca6f1e9a6c784db0e899bcc
CharlieHL <charliejones@hoarelea.com>
2024-05-10 10:29:02 +0100

    rem

--- RevitBuildChecker/Create_Deployment.bat
+++
@@ -1,6 +0,0 @@
-REM sign the dll then zip the folder
-echo update this script for password
-pause
-REM comment for password
-REM signtool.exe sign /fd SHA256 /f CharlieJones_CodeSigning.pfx /p  PASSWORD dist/RevitBuildChecker.dll
-"C:\Program Files\7-Zip\7z.exe" a -t7z dist.7z dist\ -aoa
