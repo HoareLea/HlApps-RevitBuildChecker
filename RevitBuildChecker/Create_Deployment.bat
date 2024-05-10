2e7d5d7bf8cc29b904ada742571edbecc499d7d9
CharlieHL <charliejones@hoarelea.com>
2024-05-10 10:17:38 +0100

    comment

--- RevitBuildChecker/Create_Deployment.bat
+++ RevitBuildChecker/Create_Deployment.bat
@@ -1,2 +1,3 @@
+REM sign the dll then zip the folder
 signtool.exe sign /fd SHA256 /f CharlieJones_CodeSigning.pfx /p  AegOy!2V9Z?6 dist/RevitBuildChecker.dll
 "C:\Program Files\7-Zip\7z.exe" a -t7z dist.7z dist\ -aoa
